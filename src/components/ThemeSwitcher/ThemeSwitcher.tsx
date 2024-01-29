5c4ee6b9f55329e1f4c9a6a17cdabcf934a1f1fd
LoickBde <loick.baude@gmail.com>
2024-01-29 19:50:12 +0100

    feat: use ls to store theme preference

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -5,9 +5,10 @@ import { HiMoon, HiSun } from "react-icons/hi2";
 export const ThemeSwticher = () => {
   const { theme, updateTheme } = useThemeStore();
 
+  const isLight = theme === THEMES.THEME_LIGHT;
+
   const handleClick = () => {
-    const newTheme =
-      theme === THEMES.THEME_LIGHT ? THEMES.THEME_DARK : THEMES.THEME_LIGHT;
+    const newTheme = isLight ? THEMES.THEME_DARK : THEMES.THEME_LIGHT;
     updateTheme(newTheme);
   };
 
@@ -18,17 +19,17 @@ export const ThemeSwticher = () => {
         className="theme-controller"
         value={theme}
         onChange={() => handleClick()}
-      />
-
-      <HiSun
-        className="swap-off fill-current w-10 h-10"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
+        checked={!isLight}
       />
       <HiMoon
-        className="swap-on fill-current w-10 h-10"
+        className="swap-off fill-current w-10 h-10"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
+      />
+      <HiSun
+        className="swap-on fill-current w-10 h-10"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
       />
     </label>
   );
