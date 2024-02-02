b77a6d1f1ba46ef7d2002d331ee2293ec0621782
LoickBde <loick.baude@decathlon.com>
2024-02-02 17:03:45 +0100

    chore: add husky and commitlint

---
+++ commitlint.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [2, "always", ["chore", "docs", "feat", "fix", "refactor"]],
+    "scope-case": [
+      2,
+      "always",
+      [
+        "lower-case",
+        "upper-case",
+        "camel-case",
+        "kebab-case",
+        "pascal-case",
+        "sentence-case",
+        "snake-case",
+        "start-case",
+      ],
+    ],
+  },
+};
