e319f9f7811b82611dee7d45d488bd1c2280ec3f
LoickBde <loick.baude@gmail.com>
2024-02-02 23:34:19 +0100

    chore: fix publish

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
-  base: "/<REPO>/",
+  base: "/react-pokedex/",
 });
