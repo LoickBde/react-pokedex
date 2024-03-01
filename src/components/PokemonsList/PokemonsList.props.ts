86f6415b5f3fa2ad8c7b061faf834f236d086809
LoickBde <loick.baude@decathlon.com>
2024-03-01 17:27:30 +0100

    feat(home): add end of list scroll intersection

--- src/components/PokemonsList/PokemonsList.props.ts
+++ src/components/PokemonsList/PokemonsList.props.ts
@@ -3,4 +3,5 @@ import { PokemonDetailsListType, PokemonDetailsType } from "@/types";
 export type PokemonListProps = {
   pokemons: PokemonDetailsListType;
   setSelectedPokemon: (pokemon: PokemonDetailsType | null) => void;
+  lastListItemRef: (instance: Element | null) => void;
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,9 +3,11 @@ import { PokemonsList } from "@/components/PokemonsList";
 import { queriesPokeapi } from "@/queries";
 import { PokemonDetailsType } from "@/types";
 import { useQuery } from "@tanstack/react-query";
+import { useIntersectionObserver } from "@uidotdev/usehooks";
 import { useState } from "react";
 
 export const Home = () => {
+  const [lastListItemRef, lastListItemEntry] = useIntersectionObserver({});
   const [selectedPokemon, setSelectedPokemon] =
     useState<PokemonDetailsType | null>();
   const { data, isFetching, isError } = useQuery({
@@ -17,12 +19,15 @@ export const Home = () => {
 
   const pokemons = data;
 
+  console.log(lastListItemEntry?.isIntersecting);
+
   return (
     <div className="h-full flex flex-row items-stretch">
       {pokemons && pokemons.length > 0 && (
         <PokemonsList
           pokemons={pokemons}
           setSelectedPokemon={setSelectedPokemon}
+          lastListItemRef={lastListItemRef}
         />
       )}
       <LoadingError isLoading={isFetching} isError={isError} />
